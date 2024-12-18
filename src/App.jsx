f420a09a3076425dbae4f1aa954088f581ce8395
AkshayKumarMandala <akshaykumarmandala06@gmail.com>
2024-12-18 18:03:40 +0530

    debugging whether the packages are working or not

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { FaSearch } from "react-icons/fa";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
      <div className='bg-yellow-500 h-[100vh] w-[100vw] hello-world'>
       <h1> tonetales.in </h1>
+      <div><FaSearch/></div>
      </div>
      <div className='bg-red-500'>
       <h1>username</h1>
