614b60b043fe62686194c99b5495543002653ce6
varun kumar bantupalli <bantupallivarun@gmail.com>
2024-12-20 23:04:02 +0530

    deleting errors

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,7 @@ function App() {
 
    <div className='flex justify-center items-center  h-screen w-full'>
     <h1 className='text-6xl font-bold '>tonetales.in</h1>
-    <button>subscribe  how ever mmmmmm</button>
-    <h1>fffff</h1>
-    e\<h2>dddddd</h2>
-    <h3>ffffsdfs</h3>
-    <h1>reewerewerw</h1>
+
    </div>
    </>
   );
