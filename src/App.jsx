9ee148c732f4533e7a77487924fa7a1105800df2
AkshayKumarMandala <akshaykumarmandala06@gmail.com>
2024-12-20 22:43:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+   <>
+
+   <div className='flex justify-center items-center  h-screen w-full'>
+    <h1 className='text-6xl font-bold '>tonetales.in</h1>
+   </div>
+   </>
+  );
+}
+
+export default App;
